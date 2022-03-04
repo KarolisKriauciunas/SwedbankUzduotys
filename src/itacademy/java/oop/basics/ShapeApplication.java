8bd63aab953ef89e53baf75c28b619eda52cdb80
KarolisKriauciunas <karoliomeilas@gmail.com>
2022-03-04 13:29:00 +0200

    Added Task comments

--- src/itacademy/java/oop/basics/ShapeApplication.java
+++ src/itacademy/java/oop/basics/ShapeApplication.java
@@ -1,4 +1,17 @@
-package itacademy.java.oop.basics;
+//Task 3
+//        • Create a package, name: com.itacademy.java.oop.basics
+//        • Create two classes: Square and ShapeApplication.
+//        • Square has fields: length and width of Double type
+//        • ShapeApplication has: main method.
+//        • Create Square objects (more than one) in ShapeApplication and fill out all fields with data. Store shapes in an array.
+//        • Iterate through the array of the shapes and check if shape is valid (values are not negative) if any of the value is negative print them out. For valid shapes calculate area and perimeter and print it out.
+//
+//        Requirements:
+//        • Use constructors (with-args, without-args).
+//       • Use encapsulation.
+//        • Calculation must be done using methods.
+//        •	• To print out all information create the method
+        package itacademy.java.oop.basics;
 
 import java.util.ArrayList;
 
